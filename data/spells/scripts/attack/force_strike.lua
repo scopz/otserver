9ca1f86839696d5f6a99064baa82e2017e359845
scopp <osc.car2@gmail.com>
2021-07-18 23:44:26 +0200

    Strikes buff

--- data/spells/scripts/attack/force_strike.lua
+++ data/spells/scripts/attack/force_strike.lua
@@ -4,8 +4,8 @@ setCombatParam(combat, COMBAT_PARAM_BLOCKARMOR, 1)
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_MORTAREA)
 
 function onGetFormulaValues(cid, level, maglevel)
-	local base = 45
-	local variation = 10
+	local base = 55
+	local variation = 15
 
 	local min = math.max((base - variation), ((3 * maglevel + 2 * level) * (base - variation) / 100))
 	local max = math.max((base + variation), ((3 * maglevel + 2 * level) * (base + variation) / 100))
--- data/spells/scripts/attack/ice_strike.lua
+++ data/spells/scripts/attack/ice_strike.lua
@@ -3,8 +3,8 @@ setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_HITBYICE)
 
 function onGetFormulaValues(cid, level, maglevel)
-	local base = 45
-	local variation = 10
+	local base = 55
+	local variation = 15
 
 	local min = math.max((base - variation), ((3 * maglevel + 2 * level) * (base - variation) / 100))
 	local max = math.max((base + variation), ((3 * maglevel + 2 * level) * (base + variation) / 100))
